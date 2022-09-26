cf326678e77c4a3d50dea812f28c880e12d54888
FingerPrintGames <kevinjordan199449@gmail.com>
2022-09-26 07:50:06 -0400

    fix down direction issue

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const gridContainer = document.querySelector(".grid__container");
 const gridUnit = `<div class="grid__unit"></div>`;
-const player = `<div class="player"></div>`;
+const playerElement = `<div class="player"></div>`;
+
+const playerState = {
+  direction: "left",
+  size: 1,
+};
+const gameSpeed = 200;
 
 let grid = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -23,45 +29,61 @@ function renderGrid(grid) {
         gridContainer.insertAdjacentHTML("beforeend", gridUnit);
       }
       if (grid[row][collumn] === 1) {
-        gridContainer.insertAdjacentHTML("beforeend", player);
+        gridContainer.insertAdjacentHTML("beforeend", playerElement);
       }
     }
   }
 }
 
-function movePlayer(grid, direction) {
+function movePlayer(grid, player) {
   for (let row = 0; row < grid.length; row++) {
     for (let collumn = 0; collumn < grid[row].length; collumn++) {
       if (grid[row][collumn] === 1) {
-        if (direction === "left" && collumn > 0) {
-          grid[row][collumn] = 0;
+        if (player.direction === "left" && collumn > 0) {
           grid[row][collumn - 1] = 1;
-        }
-        if (direction === "right" && collumn < 9) {
           grid[row][collumn] = 0;
+          renderGrid(grid);
+          return;
+        } else if (player.direction === "right" && collumn < 9) {
           grid[row][collumn + 1] = 1;
-        }
-        if (direction === "up" && row > 0) {
           grid[row][collumn] = 0;
+          renderGrid(grid);
+          return;
+        } else if (player.direction === "up" && row > 0) {
           grid[row - 1][collumn] = 1;
-        }
-        if (direction === "down" && row < 9) {
           grid[row][collumn] = 0;
+          renderGrid(grid);
+          return;
+        } else if (player.direction === "down" && row < 9) {
           grid[row + 1][collumn] = 1;
+          grid[row][collumn] = 0;
+          renderGrid(grid);
+          return;
         }
-        break;
       }
     }
   }
-  renderGrid(grid);
+}
+
+function changeDirection(direction) {
+  playerState.direction = direction;
 }
 
 renderGrid(grid);
 
 document.addEventListener("keydown", (event) => {
   console.log(event.key);
-  if (event.key === "ArrowLeft") movePlayer(grid, "left");
-  if (event.key === "ArrowRight") movePlayer(grid, "right");
-  if (event.key === "ArrowUp") movePlayer(grid, "up");
-  if (event.key === "ArrowDown") movePlayer(grid, "down");
+  if (event.key === "ArrowLeft") changeDirection("left");
+  if (event.key === "ArrowRight") changeDirection("right");
+  if (event.key === "ArrowUp") changeDirection("up");
+  if (event.key === "ArrowDown") changeDirection("down");
 });
+
+function step() {
+  movePlayer(grid, playerState);
+  setTimeout(() => {
+    step();
+  }, gameSpeed);
+}
+
+step();
